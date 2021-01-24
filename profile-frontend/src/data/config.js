71e89d12600d74ff01372dda7dfd77aea5e5e622
Somya Gandhi <somyagandhi03@gmail.com>
2021-01-25 01:19:23 +0530

    My Portfolio Project

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Somya Gandhi",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Somya Gandhi and I'm a B.Tech Student!",
+  githubUrl: "https://github.com/SomyaGandhi",
+  linkedinUrl: "https://www.linkedin.com/in/somya-gandhi-b778b0155/",
+  cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
+};
